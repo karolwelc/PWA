ad6a82ce8341d97297185196b12f2de80374aa17
SNEEZxd <88846720+karolwelc@users.noreply.github.com>
2025-04-25 22:51:43 +0200

    app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/serviceworker.js')
       .then(() => console.log('SW zarejestrowany!'));
   }
   
